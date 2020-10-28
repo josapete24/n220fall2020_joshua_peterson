c6e6484ef394fe1f302f8f48a15403ea5a246d84
josapete24 <jowens1678@yahoo.com>
2020-10-28 11:57:05 -0400

    Progress on the checker game

--- labs/board_game/js/app.js
+++ labs/board_game/js/app.js
@@ -1,23 +1,48 @@
-var gold_piece = document.getElementById("gold_piece1");
-var x = $(gold_piece).position();
+// Grabs the pieces and spaces //
+let gold_piece = document.getElementsByClassName("gold_piece");
+let purple_piece = document.getElementsByClassName("purple_piece");
+let gold_space = document.getElementsByClassName("gold_space");
+let purple_space = document.getElementsByClassName("purple_space");
 
-var board = document.getElementById("board");
-board.addEventListener("click", getClickPosition, false);
+// Turn and score for the players //
+let turn = "gold";
+let gold_score = 12;
+let purple_score = 12;
 
+// The outline of the chekcerboard //
+const checker_board = [
+    null, 0 , null, 1, null, 2, null, 3,
+    4, null, 5, null, 6, null, 7, null, 
+    null, 8, null, 9, null, 10, null, 11,
+    null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null,
+    12, null, 13, null, 14, null, 15, null, 
+    null, 16, null, 17, null, 18, null, 19,
+    20, null, 21, null, 22, null, 23, null
+]
 
-
-gold_piece.onclick = function() {
-
-    console.log(x);
+// Making sure that the document is grabbing everything //
+window.onload = function() {
+    console.log(gold_piece.length);
+    console.log(purple_piece.length);
+    console.log(purple_space.length);
+    console.log(gold_space.length);
 }
 
-function getClickPosition(e) {
-    var position = $(e.currentTarget).position();
-    var xPosition = e.clientX - position.x - (gold_piece.clientWidth / 2);
-    var yPosition = e.clientY - position.y - (gold_piece.clientHeight / 2);
+// Give each piece a function //
+for (i = 0; i < gold_piece.length; i++) {
+        gold_piece[i].addEventListener("click" , grabPiece());
+}
 
+for (i = 0; i < purple_piece.length; i++) {
+        purple_piece[i].addEventListener("click" , grabPiece());
+}
 
-    console.log($(e.currentTarget).position());
-    gold_piece.style.left = xPosition;
-    gold_piece.style.top = yPosition;
+// Grabs the piece the player clicked on //
+function grabPiece() {
+    if (turn == "red") {
+        playerPiece = gold_piece;
+    } else {
+        playerPiece = purple_piece;
+    }
 }
